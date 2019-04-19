a9284ad27b57b7eedaca410f0e0674d38285da94
kumaOhm <kuma.ohm@gmail.com>
2019-04-19 20:35:38 +0700

    init react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import thumbnail from './rilak_profile.jpg';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-6">
+							<div className="card mt-3 mx-auto w-75">
+								<img className="card-img-top" src={thumbnail} alt="Kuma Ohm" />
+								<div className="card-body">
+									<h5 className="card-title">Kuma Ohm</h5>
+									<h6 className="card-sub-title">I'm no one.</h6>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+							<div className="card mt-3 mx-auto w-75">
+								<div className="card-body">
+									<h5 className="card-title">Profile</h5>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+							<div className="card mt-3 mx-auto w-75">
+								<div className="card-body">
+									<h5 className="card-title">Technical Skills</h5>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+							<div className="card mt-3 mx-auto w-75">
+								<div className="card-body">
+									<h5 className="card-title">Language Skills</h5>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="card mt-3 mx-auto w-75">
+								<div className="card-body">
+									<h5 className="card-title">Experience</h5>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+							<div className="card mt-3 mx-auto w-75">
+								<div className="card-body">
+									<h5 className="card-title">Education</h5>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+							<div className="card mt-3 mx-auto w-75">
+								<div className="card-body">
+									<h5 className="card-title">Interests</h5>
+									<p className="card-text">xxxxxxxx</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
